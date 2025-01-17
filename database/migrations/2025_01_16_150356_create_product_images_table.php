3b79647c29612718e28b7a3771603b6fa2f40dab
Irfanafifromzi <irfanafifiromzi@gmail.com>
2025-01-17 17:45:45 +0800

    Updated migrations and related files

--- database/migrations/2025_01_16_150356_create_product_images_table.php
+++ database/migrations/2025_01_16_150356_create_product_images_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->unsignedBigInteger('product_id');
+            //define the foreign key relationship and casca
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
